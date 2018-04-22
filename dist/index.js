936079825c5ad4b2dd788fd2066f4c6bc681ee1f
stanyudzitski <stas.yudzitski@yandex.ru>
2018-04-22 14:12:57 +0300

    Nanolib starter exports changes vol.3

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 // import DeepFlat from "deep-flat";
@@ -14,5 +18,6 @@ function getFlatArray(arr) {
   };
   return deepFlatten;
 }
-module.exports(getFlatArray);
+
+exports.getFlatArray = getFlatArray;
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ function getFlatArray(arr) {
   const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
   return deepFlatten;
 }
-module.exports(getFlatArray);
+
+export {getFlatArray};
